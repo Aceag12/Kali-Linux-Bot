61c9f27f2d56f998e34ed31a5945803e54620acd
Aceag12 <46562212+Aceag12@users.noreply.github.com>
2019-02-27 19:55:23 +0545

    Update codes.py

--- codes.py
+++ codes.py
@@ -413,9 +413,9 @@ async def on_member_remove(member):
     for channel in member.server.channels:
         if channel.name == '★彡-welcome-bye彡★':
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(title=f'{member.name} just left {member.server.name}', description='Bye bye! We will miss you..', color = discord.Color((r << 16) + (g << 8) + b))
+            embed = discord.Embed(title=f'**{member.name}** just left **{member.server.name}**', description='Bye bye! We will miss you.', color = discord.Color((r << 16) + (g << 8) + b))
             embed.add_field(name='__User left__', value='**Hope you will be back soon.**', inline=True)
-            embed.add_field(name='Your join position was', value=member.joined_at)
+            embed.add_field(name='__Your join position was__', value=member.joined_at)
             embed.set_thumbnail(url=member.avatar_url)
             await client.send_message(channel, embed=embed)        
 @client.command(pass_context=True)
